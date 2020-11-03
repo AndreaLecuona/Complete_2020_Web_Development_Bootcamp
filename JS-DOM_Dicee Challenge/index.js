8f235145992b01f63b8987da1870a7704d4eaecc
AndreaLecuona <68034479+AndreaLecuona@users.noreply.github.com>
2020-11-03 16:15:26 -0300

    Add files via upload

---
+++ JS-DOM_Dicee Challenge/index.js
@@ -0,0 +1,16 @@
+//randomizando dados
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+document.querySelector('.img1').setAttribute("src", "images/dice" + randomNumber1 + ".png");
+
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+document.querySelector('.img2').setAttribute("src", "images/dice" + randomNumber2 + ".png");
+
+//cambios en el título
+if(randomNumber1 > randomNumber2){
+    document.querySelector('h1').innerHTML = "Player 1 wins! 🚩";
+} else if(randomNumber1 < randomNumber2){
+    document.querySelector('h1').innerHTML = "Player 2 wins! 🚩";
+} else {
+    document.querySelector('h1').innerHTML = "Draw! 🚩";
+}
+
